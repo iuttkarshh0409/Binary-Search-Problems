287140c6a6542244c7619c2dfe3287c636301b72
Utkarsh Dubey <dubeutkarsh7@gmail.com>
2024-07-13 22:14:05 +0530

    Problem No. 1

---
+++ Problem1.cpp
@@ -0,0 +1,26 @@
+//Problem 1: To seach index of key
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {2, 4, 0, 1, 9};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int key = 1;
+    int result = linearSearch(arr, size, key);
+    
+    if(result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
