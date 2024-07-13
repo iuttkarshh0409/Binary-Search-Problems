224f107d2885b0c26d265520f3475f47745285d4
Utkarsh Dubey <dubeutkarsh7@gmail.com>
2024-07-13 22:19:59 +0530

    Problem No. 3

--- Problem3.cpp
+++ Problem3.cpp
@@ -1,1 +1,30 @@
+//Problem 3: to find last occurence of key
+#include <iostream>                                                                                                                           
+using namespace std;
+
+int lastOccurence(int arr[], int size, int key) {
+    int lastIndex=-1;
+    for (int i = 0; i< size; i++) {
+        if (arr[i] == key) {
+            lastIndex= i; 
+        }
+    }
+
+    return lastIndex; 
+}
+
+int main() {
+    int i;
+    int arr[] = {7,5,0,1,7};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int key = 7;
+    int lastIndex = lastOccurence(arr, size, key);
+    
+    if(lastIndex != -1)
+        cout << "Element last occured  at index: " << lastIndex << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
 
