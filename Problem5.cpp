ef8a1c2c3cfc8706606538a9522603300f97cbe0
Utkarsh Dubey <dubeutkarsh7@gmail.com>
2024-07-13 22:23:37 +0530

    Problem No. 5

--- Problem5.cpp
+++ Problem5.cpp
@@ -1,1 +1,26 @@
+//Problem to find smallest element of array
+#include <iostream>                                                                                                                           
+using namespace std;
+
+int larOcurence(int arr[], int size) {
+    int max=arr[0];
+    for (int i = 1; i< size; i++) {
+        if (arr[i] > max) {
+            max=arr[i]; 
+        }
+        
+    }
+
+    return max; 
+}
+
+int main() {
+    int arr[] = {7,5,2,1,7};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int max = larOcurence(arr, size);
+    
+        cout << "Largest Element of array is: " << max << endl;
+
+    return 0;
+}
 
