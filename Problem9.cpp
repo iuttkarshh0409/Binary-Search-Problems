9d5504cbe0348d5ca33bdf3a0c51f4f623e26e22
Utkarsh Dubey <dubeutkarsh7@gmail.com>
2024-07-15 19:26:14 +0530

    Problem No. 9

--- Problem9.cpp
+++ Problem9.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-int searchInRotatedArray(int arr[], int size, int target) {
-    int left = 0, right = size - 1;
+vector<pair<int, int>> findPairsWithSum(int arr[], int size, int target) {
+    unordered_map<int, int> map;
+    vector<pair<int, int>> result;
     
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
-        if (arr[mid] == target)
-            return mid;
-        
-        // Check if the left half is sorted
-        if (arr[left] <= arr[mid]) {
-            // Check if the target is in the left half
-            if (arr[left] <= target && target < arr[mid])
-                right = mid - 1;
-            else
-                left = mid + 1;
+    for (int i = 0; i < size; i++) {
+        int complement = target - arr[i];
+        if (map.find(complement) != map.end()) {
+            result.push_back({arr[i], complement});
         }
-        // Right half is sorted
-        else {
-            // Check if the target is in the right half
-            if (arr[mid] < target && target <= arr[right])
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
+        map[arr[i]] = i;
     }
     
-    return -1; // Element not found
+    return result;
 }
-
