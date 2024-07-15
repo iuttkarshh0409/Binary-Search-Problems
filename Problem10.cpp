a90a7b46047e7e21f60a5c41531d25a554267498
Utkarsh Dubey <dubeutkarsh7@gmail.com>
2024-07-15 19:26:55 +0530

    Problem No. 10

--- Problem10.cpp
+++ Problem10.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>> findPairsWithSum(int arr[], int size, int target) {
+    unordered_map<int, int> map;
+    vector<pair<int, int>> result;
+    
+    for (int i = 0; i < size; i++) {
+        int complement = target - arr[i];
+        if (map.find(complement) != map.end()) {
+            result.push_back({arr[i], complement});
+        }
+        map[arr[i]] = i;
+    }
+    
+    return result;
+}
 
